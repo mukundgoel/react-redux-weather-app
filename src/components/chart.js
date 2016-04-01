90f2c7b41f68e84833ba0d29f4df7cd5fa121b7a
“mgoel1” <mgoel@walmartlabs.com>
2016-04-01 09:28:45 -0700

    refractor chart component

---
+++ src/components/chart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Component } from 'react';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
+
+export default (props) => {
+  return (
+    <div>
+      <Sparklines height={ 120 } width={ 180 } data={ props.data }>
+        <SparklinesLine color={ props.color } />
+      </Sparklines>
+    </div>
+  );
+}
