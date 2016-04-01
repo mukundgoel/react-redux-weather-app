a56e90788f2471a96877299ab8c7d7ad0fa9433a
“mgoel1” <mgoel@walmartlabs.com>
2016-04-01 08:45:39 -0700

    move enums to a seperate file

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,10 +1,17 @@
 
+import { FETCH_WEATHER } from "../enums/enums"
+
 // remember reducers are always functions
 // first argument is the state for this state this reducer is responsible for
 // section argument is the action
-export default function(state = null, action) {
-  console.log('Action received:', action);
-  console.log('State is: ', state);
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // not we cannot do return state.push(action.payload.data) since that mutates existing state object
+      // concat on the other hand will return a new state object with the old values
+      return [ action.payload.data, ...state ]; // [ city, city, city ] NOT [ city, [city, city] ]
+      // or return state.concat([action.payload.data])
+  }
 
   return state;
 }
